2fff1b487a54340486a827e1282e933f1d9a4dae
Keita Nagara <nagara.keita@gmail.com>
2021-10-04 07:02:59 +0900

    Complete Todo

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,11 @@ export const App = () => {
     setIncompleteTodos(newTodos);
     setTodoText("");
   };
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
   return (
     <>
       <div className="input-area">
@@ -21,12 +26,12 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
           })}
