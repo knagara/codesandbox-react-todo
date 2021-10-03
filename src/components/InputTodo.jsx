2cd09059718b1ba4df29ac180b0d431ccf9d6b10
Keita Nagara <nagara.keita@gmail.com>
2021-10-04 07:28:27 +0900

    fix

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 
 export const InputTodo = (props) => {
-  const { todoText, onChange, onClick } = props;
+  const { todoText, onChange, onClick, disabled } = props;
   return (
     <div className="input-area">
-      <input type="text" value={todoText} onChange={onChange} />
-      <button onClick={onClick}>追加</button>
+      <input
+        disabled={disabled}
+        type="text"
+        value={todoText}
+        onChange={onChange}
+      />
+      <button disabled={disabled} onClick={onClick}>
+        追加
+      </button>
     </div>
   );
 };
